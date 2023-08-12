6b179c08868d0a583af816308f7bfa6b0b1856b7
janing <jhonmichaelturija@gmail.com>
2023-08-12 19:47:56 +0800

    update

--- api/views.py
+++ api/views.py
@@ -5,27 +5,33 @@ from .models import Barangay
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+import json
 
 class BarangayViewSet(viewsets.ModelViewSet):
-    queryset = Barangay.objects.all().order_by('-barangay')
+    queryset = Barangay.objects.all().order_by('barangay')
     serializer_class = BarangaySerializer
 
 
 def index(request):
-     return render(request, 'api/index.html')
+    barangays = Barangay.objects.all().order_by('barangay')
+    serializer = BarangaySerializer(barangays, many=True)
+    return render(request, 'api/index.html',{'data': json.dumps(serializer.data)})
  
 class SingleBarangayGeoJSONView(APIView):
-    def get(self, request, code, *args, **kwargs):
+    def get(self, request, code):
         barangay = get_object_or_404(Barangay, code=code)
         serializer = BarangaySerializer(barangay)
         return Response(serializer.data)
 
 
 class AllBarangayGeoJSONView(APIView):
-    def get(self, request, *args, **kwargs):
-        barangays = Barangay.objects.all().order_by('-barangay')
+    def get(self, request):
+        barangays = Barangay.objects.order_by('barangay')
         serializer = BarangaySerializer(barangays, many=True)
         return Response(serializer.data)
+    
+    
+
    
 
           
