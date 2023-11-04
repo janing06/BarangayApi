8a7290a5713f62c27d1b3b8d5a572afd2a5e2336
Jhon Michael Turija <120067518+janing06@users.noreply.github.com>
2023-11-04 21:40:50 +0800

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -46,7 +46,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'core.urls'
 
